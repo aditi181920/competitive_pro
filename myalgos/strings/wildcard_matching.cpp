543fbbb689ad83fd0428c366f7a3ca311efdde16
Aditi <94597499+hackapie@users.noreply.github.com>
2021-12-20 18:05:14 +0530

    Standard wildcard pattern matching problem
    Problem:
    Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
    
    '?' Matches any single character.
    '*' Matches any sequence of characters (including the empty sequence).
    The matching should cover the entire input string (not partial).

--- myalgos/strings/wildcard_matching.cpp
+++ myalgos/strings/wildcard_matching.cpp
@@ -1,1 +1,13 @@
+BASE CASES:
+dp[i][j] = matching result of s[0...i) and p[0...j)
+dp[0][0] = true
+dp[i][0] = false for i>0   //since empty pattern can match empty string only
+dp[0][j] = dp[0][j-1] && p[j-1]=='*'    //since only sequence of empty pattern can match empty string
 
+TRANSITIONS:
+if p[j-1]=='*'  //we can match any number of end characters of s so , dp[i][j] = (dp[i][j-1]||dp[i-1][j-1]||dp[i-2][j-1]||dp[i-3][j-1]||....||dp[0][j-1])
+                                             //dp[i-1][j] = (dp[i-1][j-1]||dp[i-2][j-1]||....||dp[0][j-1])
+    dp[i][j]=(dp[i][j-1]||dp[i-1][j])
+else:
+    dp[i][j]=(dp[i-1][j-1] && (p[j-1]=='?' || (p[j-1]==s[i-1])))    //except *, only ? and any character can be present which leads to these two cases being formed
+                                             
